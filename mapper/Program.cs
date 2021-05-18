b5ad9bfbea6cdde6c518dff511faf5856a2d45f8
benjamin <riders_23x@hotmail.com>
2021-05-17 21:02:10 -0500

    Agregar archivos de proyecto.

---
+++ mapper/Program.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+           
+            departamento depa = new departamento();
+            depa.correo = "benja";
+            depa.nombre = "pepe";
+
+            distrito dis = new distrito();
+
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<departamento, distrito>();
+            });
+
+            IMapper iMapper = config.CreateMapper();
+            dis = iMapper.Map<departamento, distrito>(depa);
+
+            Console.Read();
+            //Mapper.CreateMap<departamento, distrito>();
+        }
+
+    }
+
+    class departamento
+    {
+
+        public string correo { get; set; }
+        public string nombre { get; set; }
+
+
+    }
+
+    class distrito
+    {
+        public string correo { get; set; }
+        public string nombre { get; set; }
+
+        public string habitantes { get; set; }
+        public string cantidad { get; set; }
+
+    }
+}
---
+++ mapper/Tares.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mapper
+{
+    class Tares
+    {
+        static async Task Main(string[] args)
+        {
+            // task independiente para ejecutar tareas asincronas
+            // task 
+            // async y await para metodos asincronos
+            // await valida si la tarea se a terminado
+            var tarea = new Task(() =>
+            {
+                Thread.Sleep(4000);
+                Console.WriteLine("asincrona tarea 1");
+            }
+            );
+            tarea.Start();
+
+            var tarea2 = new Task(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("asincrona tarea 2");
+            }
+            );
+            tarea2.Start();
+            await tarea;
+            await tarea2;
+            int res = await prueba();
+            Console.WriteLine(res);
+
+            //await tarea;
+            Console.WriteLine("Primera tarea");
+            Console.ReadLine();
+        }
+
+        static async Task<int> prueba ()
+        {
+            var tarea = new Task<int>(() => new Random().Next(1000));
+            tarea.Start();
+            int resul =await tarea;
+            return resul;
+
+        }
+    }
+}
